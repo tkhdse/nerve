95daf3ebbb0b56897e007082bf66eb0fcc1d6f89
Tanish <tkhadse2@illinois.edu>
2025-01-03 00:02:20 -0600

    blog progress

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -5,8 +5,13 @@ const Blog = () => {
     return (
         <div>
             <Navbar />
-            <div class="text-6xl w-[60%] h-[60vh] mt-[10%] ml-[20%] text-center">
-                What's new with Nerve?
+            <div class=" w-[60%] h-[55vh] mt-[10%] mb-[5%] ml-[20%]">
+                <div class="text-6xl text-center mb-[50px]">
+                    What's new with Nerve?
+                </div>
+                {/* <div class="bg-white rounded-md border-gray-600 border-[2px] ml-[20%] w-[60%]">
+                    yo
+                </div> */}
             </div>
             <Footer />
         </div>
