eae1fdec9a7c831340be11e84912f640890a198d
Tanish <tkhadse2@illinois.edu>
2025-01-02 18:59:23 -0600

    added clouds to main page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import './App.css';
+import { useState, useEffect } from 'react'
 import Logo from './components/Logo';
+import GenerateClouds from './components/Animation';
 
+
+const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize); 
+  }, []);
+
+  return windowDimensions;
+};
 
 function App() {
+  const { width, height } = useWindowDimensions();
+
   return (
     <div class="bg-[#FFFFF2] font-instrument  ">
       <div class="text-center">
         <Logo color={"cream"} showText={true} textColor={false}/>
       </div>
 
-      <div class="text-6xl w-[60%] h-[60vh] text-center ml-[20%] mt-[10%]">
+      <GenerateClouds width={width} height={height*0.8} />
+
+      <div class="text-6xl w-[60%] h-[60vh] text-center ml-[20%] mt-[10%] z-10">
         The Future of Energy-Efficient Computing
       </div>
 
